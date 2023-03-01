40093225f8ca45eaf0fd8a5abc563c1d8a6218b0
Michael <michaeltheisen@hotmail.com>
2023-03-01 11:10:40 -0800

    Adjusted Ghost

--- enemies.js
+++ enemies.js
@@ -1,16 +1,16 @@
 class Ghost{
     constructor(game , x, y){
         Object.assign(this,{game, x, y});
-        this.x=700;
+        this.x=1500;
         this.y=500;
         this.velocity = { x: 0, y:0};
         this.spritesheet = ASSET_MANAGER.getAsset("./Sprites/ghost.png");
-        this.speed = 100;
+        this.speed = 200;
         this.state = "FLOAT";
         this.animationList = [];  
         this.dead = false;     
-                                                  //spritesheet,xStart, yStart, width, height, frameCount, frameDuration, scale)
-        this.animationList["FLOAT"] = new Animator(ASSET_MANAGER.getAsset("./Sprites/ghost.png"), 10, 0, 57.5, 65, 18, 0.07, 2, 2);
+                                                  //spritesheet,xStart, yStart, width, height, frameCount, frameDuration, scale, spriteBorderWidth)
+        this.animationList["FLOAT"] = new Animator(ASSET_MANAGER.getAsset("./Sprites/ghost.png"), 10, 0, 57.5, 65, 18, 0.07, 1.5, 2);
     }
 
     
